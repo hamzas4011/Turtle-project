934f5e1ef655303c59a1255052053ca7c96cc57c
hamzas4011 <hamzas4011@hotmail.com>
2023-10-11 18:10:12 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+
+
+#Importing turtle libary and creating objects and making a circle
+
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("blue")
+timmy.forward(200)
+timmy.left(410)
+timmy.right(410)
+timmy.backward(60)
+timmy.circle(160,360)
+
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
